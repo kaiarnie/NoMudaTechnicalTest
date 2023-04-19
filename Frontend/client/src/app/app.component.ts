2e48c7a6dd252c5f3be2cff5bae6585417cb395a
Kai Arnold <kaiarnie19@gmail.com>
2023-04-19 10:51:26 +0100

    Task 1 - Created a basic list that allows for addition and deletion of items

--- Frontend/client/src/app/app.component.ts
+++ Frontend/client/src/app/app.component.ts
@@ -12,6 +12,9 @@ declare var google: any;
 })
 export class AppComponent implements AfterViewInit {
   title = 'Angular13, DotNet Core 6 API, Google Auth';
+  item!: string;
+  shoppingList: any[] = [];
+  
   constructor(private http: HttpClient, public authService: AuthService) { 
     this.authService.checkToken();
   }
@@ -25,6 +28,44 @@ export class AppComponent implements AfterViewInit {
       document.getElementById("buttonDiv"), // Points to the button in the front end
       { size: "large", type: "icon", shape: "pill" } // Change the Google button's look
     );
+  }
 
+  //onClick method adds item to shoppingList array
+  onClick() {
+    const idValue = this.shoppingList.length + 1;  //idValue is the length of the array + 1 as array starts from 0, this count begins at 1
+    var shoppingItem = new ShoppingListItem(idValue, this.item, false)
+    this.shoppingList.push(shoppingItem);
+    console.log(this.item);
+    console.log(this.shoppingList);
+  }
+
+  //Empties Array
+  clear() {
+    this.shoppingList.length = 0;
+    console.log(this.shoppingList);
+  }
+
+  //Deletes item from array
+  deleteItem(itemName: string) {
+    const index = this.shoppingList.findIndex(shoppingItem => shoppingItem.itemName === itemName); //Finds index is shoppingItem in array via its class property name. Allows for specific item deletion
+    console.log(index);
+    if (index > -1) {
+      this.shoppingList.splice(index, 1);
+    }
+  }
+
+}
+
+//Class for ShoppingList Item
+class ShoppingListItem {
+
+  id: number;
+  itemName: string;
+  important: boolean;
+  
+  constructor(id: number, itemName: string, important: boolean ) {
+    this.id = id;
+    this.itemName = itemName;
+    this.important = important;
   }
 }
--- Frontend/client/src/app/app.module.ts
+++ Frontend/client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,7 +14,8 @@ import { APP_PROVIDERS } from './app.providers';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     ...APP_PROVIDERS
